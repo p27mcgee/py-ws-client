cd1b794877d752ea10f4d713d703d503b168e144
Phil McGee <p27mcgee@users.noreply.github.com>
2020-06-22 00:22:48 -0400

    tweaks

--- LinkExtractor.py
+++ LinkExtractor.py
@@ -1,5 +1,5 @@
 from html.parser import HTMLParser
-from html.entities import name2codepoint
+
 
 class LinkExtractor(HTMLParser):
 
@@ -8,42 +8,10 @@ class LinkExtractor(HTMLParser):
         super().__init__()
 
     def handle_starttag(self, tag, attrs):
-        # print("Start tag:", tag)
-        # for attr in attrs:
-        #     print("     attr:", attr)
         if tag == 'a':
             for name, val in attrs:
                 if name == 'href':
                     self.hrefs.append(val)
-
-    def handle_endtag(self, tag):
-        # print("End tag  :", tag)
-        pass
-
-    def handle_data(self, data):
-        # print("Data     :", data)
-        pass
-
-    def handle_comment(self, data):
-        # print("Comment  :", data)
-        pass
-
-    def handle_entityref(self, name):
-        # c = chr(name2codepoint[name])
-        # print("Named ent:", c)
-        pass
-
-    def handle_charref(self, name):
-        # if name.startswith('x'):
-        #     c = chr(int(name[1:], 16))
-        # else:
-        #     c = chr(int(name))
-        # print("Num ent  :", c)
-        pass
-
-    def handle_decl(self, data):
-        # print("Decl     :", data)
-        pass
 
     def extract_hrefs(self, html):
         self.feed(html)
@@ -76,82 +44,6 @@ body {
 <a href="android/" title="android/">android/</a>                                                         -         -      
 <a href="aws/" title="aws/">aws/</a>                                                             -         -      
 <a href="batch/" title="batch/">batch/</a>                                                           -         -      
-<a href="beandoc/" title="beandoc/">beandoc/</a>                                                         -         -      
-<a href="boot/" title="boot/">boot/</a>                                                            -         -      
-<a href="build/" title="build/">build/</a>                                                           -         -      
-<a href="cloud/" title="cloud/">cloud/</a>                                                           -         -      
-<a href="commons/" title="commons/">commons/</a>                                                         -         -      
-<a href="credhub/" title="credhub/">credhub/</a>                                                         -         -      
-<a href="data/" title="data/">data/</a>                                                            -         -      
-<a href="flex/" title="flex/">flex/</a>                                                            -         -      
-<a href="geode/" title="geode/">geode/</a>                                                           -         -      
-<a href="guice/" title="guice/">guice/</a>                                                           -         -      
-<a href="hateoas/" title="hateoas/">hateoas/</a>                                                         -         -      
-<a href="integration/" title="integration/">integration/</a>                                                     -         -      
-<a href="javaconfig/" title="javaconfig/">javaconfig/</a>                                                      -         -      
-<a href="kafka/" title="kafka/">kafka/</a>                                                           -         -      
-<a href="ldap/" title="ldap/">ldap/</a>                                                            -         -      
-<a href="maven/" title="maven/">maven/</a>                                                           -         -      
-<a href="metrics/" title="metrics/">metrics/</a>                                                         -         -      
-<a href="mobile/" title="mobile/">mobile/</a>                                                          -         -      
-<a href="osgi/" title="osgi/">osgi/</a>                                                            -         -      
-<a href="plugin/" title="plugin/">plugin/</a>                                                          -         -      
-<a href="restdocs/" title="restdocs/">restdocs/</a>                                                        -         -      
-<a href="retry/" title="retry/">retry/</a>                                                           -         -      
-<a href="roo/" title="roo/">roo/</a>                                                             -         -      
-<a href="security/" title="security/">security/</a>                                                        -         -      
-<a href="session/" title="session/">session/</a>                                                         -         -      
-<a href="shell/" title="shell/">shell/</a>                                                           -         -      
-<a href="social/" title="social/">social/</a>                                                          -         -      
-<a href="spring/" title="spring/">spring/</a>                                                          -         -      
-<a href="spring-agent/" title="spring-agent/">spring-agent/</a>                                                    -         -      
-<a href="spring-aop/" title="spring-aop/">spring-aop/</a>                                                      -         -      
-<a href="spring-asm/" title="spring-asm/">spring-asm/</a>                                                      -         -      
-<a href="spring-aspects/" title="spring-aspects/">spring-aspects/</a>                                                  -         -      
-<a href="spring-beandoc/" title="spring-beandoc/">spring-beandoc/</a>                                                  -         -      
-<a href="spring-beans/" title="spring-beans/">spring-beans/</a>                                                    -         -      
-<a href="spring-binding/" title="spring-binding/">spring-binding/</a>                                                  -         -      
-<a href="spring-context/" title="spring-context/">spring-context/</a>                                                  -         -      
-<a href="spring-context-indexer/" title="spring-context-indexer/">spring-context-indexer/</a>                                          -         -      
-<a href="spring-context-support/" title="spring-context-support/">spring-context-support/</a>                                          -         -      
-<a href="spring-core/" title="spring-core/">spring-core/</a>                                                     -         -      
-<a href="spring-dao/" title="spring-dao/">spring-dao/</a>                                                      -         -      
-<a href="spring-expression/" title="spring-expression/">spring-expression/</a>                                               -         -      
-<a href="spring-framework-bom/" title="spring-framework-bom/">spring-framework-bom/</a>                                            -         -      
-<a href="spring-full/" title="spring-full/">spring-full/</a>                                                     -         -      
-<a href="spring-hibernate/" title="spring-hibernate/">spring-hibernate/</a>                                                -         -      
-<a href="spring-hibernate2/" title="spring-hibernate2/">spring-hibernate2/</a>                                               -         -      
-<a href="spring-hibernate3/" title="spring-hibernate3/">spring-hibernate3/</a>                                               -         -      
-<a href="spring-ibatis/" title="spring-ibatis/">spring-ibatis/</a>                                                   -         -      
-<a href="spring-instrument/" title="spring-instrument/">spring-instrument/</a>                                               -         -      
-<a href="spring-instrument-tomcat/" title="spring-instrument-tomcat/">spring-instrument-tomcat/</a>                                        -         -      
-<a href="spring-jca/" title="spring-jca/">spring-jca/</a>                                                      -         -      
-<a href="spring-jcl/" title="spring-jcl/">spring-jcl/</a>                                                      -         -      
-<a href="spring-jdbc/" title="spring-jdbc/">spring-jdbc/</a>                                                     -         -      
-<a href="spring-jdo/" title="spring-jdo/">spring-jdo/</a>                                                      -         -      
-<a href="spring-jms/" title="spring-jms/">spring-jms/</a>                                                      -         -      
-<a href="spring-jmx/" title="spring-jmx/">spring-jmx/</a>                                                      -         -      
-<a href="spring-jpa/" title="spring-jpa/">spring-jpa/</a>                                                      -         -      
-<a href="spring-ldap/" title="spring-ldap/">spring-ldap/</a>                                                     -         -      
-<a href="spring-messaging/" title="spring-messaging/">spring-messaging/</a>                                                -         -      
-<a href="spring-mock/" title="spring-mock/">spring-mock/</a>                                                     -         -      
-<a href="spring-ojb/" title="spring-ojb/">spring-ojb/</a>                                                      -         -      
-<a href="spring-orm/" title="spring-orm/">spring-orm/</a>                                                      -         -      
-<a href="spring-oxm/" title="spring-oxm/">spring-oxm/</a>                                                      -         -      
-<a href="spring-parent/" title="spring-parent/">spring-parent/</a>                                                   -         -      
-<a href="spring-portlet/" title="spring-portlet/">spring-portlet/</a>                                                  -         -      
-<a href="spring-remoting/" title="spring-remoting/">spring-remoting/</a>                                                 -         -      
-<a href="spring-struts/" title="spring-struts/">spring-struts/</a>                                                   -         -      
-<a href="spring-support/" title="spring-support/">spring-support/</a>                                                  -         -      
-<a href="spring-test/" title="spring-test/">spring-test/</a>                                                     -         -      
-<a href="spring-tomcat-weaver/" title="spring-tomcat-weaver/">spring-tomcat-weaver/</a>                                            -         -      
-<a href="spring-toplink/" title="spring-toplink/">spring-toplink/</a>                                                  -         -      
-<a href="spring-tuple/" title="spring-tuple/">spring-tuple/</a>                                                    -         -      
-<a href="spring-tuple-parent/" title="spring-tuple-parent/">spring-tuple-parent/</a>                                             -         -      
-<a href="spring-tx/" title="spring-tx/">spring-tx/</a>                                                       -         -      
-<a href="spring-web/" title="spring-web/">spring-web/</a>                                                      -         -      
-<a href="spring-webflow/" title="spring-webflow/">spring-webflow/</a>                                                  -         -      
-<a href="spring-webflux/" title="spring-webflux/">spring-webflux/</a>                                                  -         -      
 <a href="spring-webmvc/" title="spring-webmvc/">spring-webmvc/</a>                                                   -         -      
 <a href="spring-webmvc-portlet/" title="spring-webmvc-portlet/">spring-webmvc-portlet/</a>                                           -         -      
 <a href="spring-webmvc-struts/" title="spring-webmvc-struts/">spring-webmvc-struts/</a>                                            -         -      
--- maven-repo-client.py
+++ maven-repo-client.py
@@ -1,134 +1,187 @@
-import requests
-import re
-import json
 import csv
+import json
+import re
+
+import requests
+
 from LinkExtractor import LinkExtractor
 
-verbose = True
-include_artifact_versions = False
+SPRINGFRAMEWORK = 'org.springframework'
+SPRINGWEBFLOW = 'org.springframework.webflow'
+SPRINGBOOT = 'org.springframework.boot'
 
-base_group = 'org.springframework'
-base_group_path = base_group.replace('.', '/')
+VERBOSE = False
+INCLUDE_VERSIONS = False
+CRAWL_MAVEN = True
+CRAWL_GROUP = SPRINGFRAMEWORK
+
+JSON_FILE = 'repo.json'
+CSV_FILE = 'artifacts.csv'
+
 maven_base_url = 'https://repo1.maven.org/maven2/'
 metadata_file = 'maven-metadata.xml'
 jar_path_ext = '.jar'
 sha1_path_ext ='.jar.sha1'
 jar_name_format = '{}-{}.jar'   # artifact id, version
 
-vers_path_pattern = re.compile(r"^(ivy\-)?\d+\.\d+\.\d+[.-a-zA-Z0-9]*/?")
+vers_path_pattern = re.compile(r"^(ivy\-)?\d+\.\d+(\.\d+)?[.-a-zA-Z0-9]*/?")
 
-GROUP_NODE = "sub-group"
-ARTIFACT_NODE = "artifact"
+ABERRANT_ARTIFACT_NAMES = ['spring-session-bom', 'spring-obj']
 
-download_dir = 'data'
+GROUP_NODE = 'sub-group'
+ARTIFACT_NODE = 'artifact'
+UNPARSABLE_NODE = 'unparseable'
+
+NODE_TYPE = 'type'
+NODE_NAME = 'name'
+CHILDREN = 'children'
 
 def get_response_or_raise(url):
     response = requests.get(url)
     if response.status_code == 200:
-        if verbose:
+        if VERBOSE:
             print('GET ' + url + ' returns:')
             print(response)
+        else:
+            print('.', end='')
         return response
     else:
         msg = 'GET ' + url + ' returns HTTP status: ' + str(response.status_code)
-        if verbose:
+        if VERBOSE:
             print(msg)
         raise Exception(msg)
 
 def parse_maven_response(response):
-    body = response.content.decode("utf-8")
-    # print (body)
+    body = response.content.decode('utf-8')
     return LinkExtractor().extract_hrefs(body)
 
-def is_artifact(child_paths):
-    if metadata_file in child_paths:
+def is_artifact(child_paths, node_name):
+    if node_name in ABERRANT_ARTIFACT_NAMES:
         return True
+
+    # org.springframework.boot and .cloud both have stray metadata_files
+    # if metadata_file in child_paths:
+    #     return True
+
     for child_path in child_paths:
         if vers_path_pattern.match(child_path):
             return True
     return False
 
+last_node_type = GROUP_NODE
+
+def progress_indicator(node_type):
+    global last_node_type
+    if not VERBOSE and last_node_type != node_type:
+        last_node_type = node_type
+        print()
+
 def retrieve_and_parse(node_name, url):
-    response = get_response_or_raise(url)
-    child_paths = parse_maven_response(response)
-    if is_artifact(child_paths):
-        if include_artifact_versions:
-            return {"name": node_name, "type": ARTIFACT_NODE, "children": child_paths}
+    try:
+        response = get_response_or_raise(url)
+        child_paths = parse_maven_response(response)
+    except Exception as e:
+        print("Exception processing url " + url)
+        print(str(e))
+        return {NODE_NAME: node_name, NODE_TYPE: UNPARSABLE_NODE, CHILDREN: []}
+
+    if is_artifact(child_paths, node_name):
+        progress_indicator(ARTIFACT_NODE)
+        if INCLUDE_VERSIONS:
+            return {NODE_NAME: node_name, NODE_TYPE: ARTIFACT_NODE, CHILDREN: child_paths}
         elif metadata_file in child_paths:
-            # return {"name": node_name, "type": ARTIFACT_NODE, "children": [metadata_file]}
+            # return {NODE_NAME: node_name, NODE_TYPE: ARTIFACT_NODE, CHILDREN: [metadata_file]}
             # for brevity in the normal case where an artifact has metadata omit children
-            return {"name": node_name, "type": ARTIFACT_NODE}
+            return {NODE_NAME: node_name, NODE_TYPE: ARTIFACT_NODE}
         else:
-            # for the aberrant case with missing metadata show children empty
-            return {"name": node_name, "type": ARTIFACT_NODE, "children": []}
+            # for the aberrant case with missing metadata show empty children
+            return {NODE_NAME: node_name, NODE_TYPE: ARTIFACT_NODE, CHILDREN: []}
     else:
-        return {"name": node_name, "type": GROUP_NODE, "children": child_paths}
+        progress_indicator(GROUP_NODE)
+        return {NODE_NAME: node_name, NODE_TYPE: GROUP_NODE, CHILDREN: child_paths}
 
 def create_repo_tree(node_name, root_url):
     node_data = retrieve_and_parse(node_name, root_url)
-    if node_data["type"] == GROUP_NODE:
-        child_paths = node_data["children"]
-        node_data["children"] = []
+    if node_data[NODE_TYPE] == GROUP_NODE:
+        child_paths = node_data[CHILDREN]
+        node_data[CHILDREN] = []
         for child_path in child_paths:
             if not child_path == '../':
-                child_node = create_repo_tree(child_path.rstrip("/"), root_url + child_path)
-                node_data["children"].append(child_node)
+                child_node = create_repo_tree(child_path.rstrip('/'), root_url + child_path)
+                node_data[CHILDREN].append(child_node)
         return node_data
-    else:
+    elif node_data[NODE_TYPE] == ARTIFACT_NODE:
         # we can retrieve the metadata if we choose
         return node_data
+    elif node_data[NODE_TYPE] == UNPARSABLE_NODE:
+        return node_data
+    else:
+        raise Exception('Unknown node type: ' + str(node_data[NODE_TYPE]))
 
-def crawl_repo(groupid='org.springframework.session'):
+def crawl_repo(groupid):
     group_path = groupid.replace('.', '/')
-    group_url = maven_base_url + group_path + "/"
+    group_url = maven_base_url + group_path + '/'
     return create_repo_tree(groupid, group_url)
 
-def write_repo_tree(repo_tree, filename="repo.json"):
+def write_repo_tree(repo_tree, filename=JSON_FILE):
     jsonStr = json.dumps(repo_tree, indent=2)
-    print(jsonStr)
-    with open(filename, "w") as outfile:
+    with open(filename, 'w') as outfile:
         outfile.write(jsonStr)
     return jsonStr
 
-def read_repo_tree(filename="repo-session.json"):
+def read_repo_tree(filename=JSON_FILE):
     with open(filename, 'r') as infile:
         repo_tree = json.load(infile)
     return repo_tree
 
 def flatten_repo_node(artifact_list, parent_group, node):
-    if node['type'] == ARTIFACT_NODE:
-        artifact = node['name']
+    if node[NODE_TYPE] == ARTIFACT_NODE:
+        artifact = node[NODE_NAME]
         group = parent_group
         artifact_list.append([group, artifact])
-    elif node['type'] == GROUP_NODE:
-        group = node['name']
+    elif node[NODE_TYPE] == GROUP_NODE:
+        group = node[NODE_NAME]
         if parent_group:
             group = parent_group + '.' + group
-        for child_node in node['children']:
+        for child_node in node[CHILDREN]:
             flatten_repo_node(artifact_list, group, child_node)
+    elif node[NODE_TYPE] == UNPARSABLE_NODE:
+        name = node[NODE_NAME]
+        group = parent_group
+        artifact_list.append([group, name + '(UNPARSABLE)'])
     else:
-        raise Exception("Unknown repo_tree node type: " + str(repo_tree[type]))
+        raise Exception('Unknown node type: ' + str(node[NODE_TYPE]))
 
 def flatten_repo_tree(repo_tree):
     artifact_list = []
-    flatten_repo_node(artifact_list, "", repo_tree)
+    flatten_repo_node(artifact_list, '', repo_tree)
     return artifact_list
 
-def write_artifact_csv(artifact_list, filename='artifacts.csv'):
-    with open(filename, 'w') as outfile:
-        writer = csv.writer(outfile)
+def write_artifact_csv(artifact_list, filename=CSV_FILE):
+    with open(filename, 'w', newline='') as outfile:
+        writer = csv.writer(outfile, )
         writer.writerow(['group', 'artifact'])
         writer.writerows(artifact_list)
 
 def main():
-    print(write_repo_tree(crawl_repo(groupid='org.springframework')))
-    repo_tree = read_repo_tree()
-    # print(json.dumps(repo_tree, indent=2))
+    if CRAWL_MAVEN:
+        repo_tree = crawl_repo(CRAWL_GROUP)
+        repo_json = write_repo_tree(repo_tree)
+        if VERBOSE:
+            print(repo_json)
+    else:
+        repo_tree = read_repo_tree()
+        if VERBOSE:
+            print(json.dumps(repo_tree, indent=2))
     artifact_list = flatten_repo_tree(repo_tree)
     write_artifact_csv(artifact_list)
-    # print(json.dumps(artifact_list, indent=2))
+    if VERBOSE:
+        with open(CSV_FILE, 'r') as file:
+            lines = file.readlines()
+        for line in lines:
+            print(line, end='')
 
 if __name__ == '__main__':
     # test = ['ivy-2.4.0.RELEASE.xml.asc']
-    # print("is_artifact({}) = {}".format(test, str(is_artifact(test))))
+    # print('is_artifact({}) = {}'.format(test, str(is_artifact(test))))
     main()
